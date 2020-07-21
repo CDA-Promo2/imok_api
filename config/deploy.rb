2d6046f617f649a37a80a1a2fb5ed5d68f57b305
Sebastien HINARD <se.hinard@gmail.com>
2020-07-21 16:13:02 +0200

    added .env to gitignore

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "https://github.com/CDA-Promo2/imok_api.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/var/www/clients/client51/web49/web"
+set :deploy_to, "/var/www/clients/client51/web49/web/imok_api"
 
 namespace :deploy do
 
@@ -35,7 +35,7 @@ end
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
+append :linked_files, ".env"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
